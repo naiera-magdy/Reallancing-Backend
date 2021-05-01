b739f7260ccc9bd1cc057e38c96ae0b8251440c3
omar_salama <osalama710@gmail.com>
2021-05-01 22:02:53 +0200

    Added Proposal Model

---
+++ models/proposalModel.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const proposalSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: [true, 'Proposal must belong to a specified user.']
+  },
+  job: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Job',
+    required: [true, 'Proposal must belong to a specified job.']
+  },
+  coverLetter: {
+    type: String,
+    required: [true, 'The proposal needs to have a cover letter.'],
+    trim: true,
+    minlength: [50, 'The Cover Letter needs to have at least 50 characters.'],
+    maxlength: [1000, 'The Cover Letter must have less than 1000 characters.']
+  },
+  proposedHourlyRate: {
+    type: Number,
+    required: [true, 'A freelancer must have a proposed hourly rate'],
+    min: [10, 'Hourly Rate min value is 10 LE'],
+    max: [2000, 'Hourly Rate max value is 2000 LE']
+  }
+});
+
+const Proposal = mongoose.model('Proposal', proposalSchema);
+module.exports = Proposal;
