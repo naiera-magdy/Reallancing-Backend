8e454fed8c75f587590e102ddf3f5ddd6d02b318
naiera-magdy <44544070+naiera-magdy@users.noreply.github.com>
2021-06-06 16:44:47 +0200

    Unrestricted Categories & skills

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -3,15 +3,20 @@ const authController = require('../controllers/authController');
 const categoryController = require('../controllers/categoryController');
 
 const router = express.Router();
-router.use(authController.protectRoutes);
+// router.use(authController.protectRoutes);
 router
   .route('/')
   .get(categoryController.getAllCategories)
-  .post(authController.restrictTo('admin'), categoryController.createCategory);
+  .post(
+    authController.protectRoutes,
+    authController.restrictTo('admin'),
+    categoryController.createCategory
+  );
 router
   .route('/:id')
   .get(categoryController.getCategory)
   .delete(
+    authController.protectRoutes,
     authController.restrictTo('admin'),
     categoryController.deleteCategory
   );
