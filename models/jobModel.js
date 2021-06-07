3a7be8a0f998e200c918d801d53cc22fc633b056
ahmedx3 <ahmed.moustafaa99@gmail.com>
2021-06-07 23:33:10 +0200

    Modified a validation

--- models/jobModel.js
+++ models/jobModel.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 // const validator = require('validator');
-const idValidator = require('mongoose-id-validator');
 
 const jobSchema = new mongoose.Schema(
   {
@@ -80,11 +79,6 @@ const jobSchema = new mongoose.Schema(
     toObject: { virtuals: true }
   }
 );
-
-// Plugin to check if referenced ids exist
-jobSchema.plugin(idValidator, {
-  message: 'Bad ID value for {PATH}'
-});
 
 jobSchema.pre('save', async function(next) {
   this.createdAt = Date();
